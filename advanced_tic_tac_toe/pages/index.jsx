e60c3b0640897472a6777bbe19b783eb9f3bd964
OlegsIljanovs <42992921+CoconutTree01@users.noreply.github.com>
2024-01-09 21:13:18 +0200

    Advanced-tik-tak-toe

--- advanced_tic_tac_toe/pages/index.jsx
+++ advanced_tic_tac_toe/pages/index.jsx
@@ -1,129 +1,30 @@
-import {useState} from 'react'
-
-const SYMBOL_X = 'X';
-const SYMBOL_O = 'O';
-
-const computeWinner = (cells) => {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (
-            cells[a] &&
-            cells[a] === cells[b] &&
-            cells[a] === cells[c]
-        ) {
-            return [a, b, c]
-        }
-    }
-}
-
-
+import { Header } from "../components/header";
+import { Game } from "../components/game-new";
+import { UiTextField } from "../components/uikit/fields/ui-text-field";
+import { UiSelectField } from "../components/uikit/fields/ui-select-field";
 
 export default function HomePage() {
-    const [cells, setCells] = useState([null, null, null, null, null, null, null, null, null]);
-    const [currentStep, setCurrentStep] = useState(SYMBOL_O);
-    const [winnerSequence, setWinnerSequence] = useState();
-
-
-    const handleCellClick = (index) => {
-        if (cells[index] || winnerSequence) {
-            return;
-        }
-        const cellsCopy = cells.slice()
-        cellsCopy[index] = currentStep;
-        const winner = computeWinner(cellsCopy);
-
-
-        setCells(cellsCopy);
-        setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
-        setWinnerSequence(winner)
-    }
-
-    const handleResetClick = () => {
-        setCells(Array.from({ length: 9 }, () => null));
-        setCurrentStep(SYMBOL_X);
-        setWinnerSequence(undefined)
-    };
-
-
-
-    const winnerSymbol = winnerSequence ? cells[winnerSequence[0]] : undefined
-    const isDraw = !winnerSequence && cells.filter(value => value).length === 9;
-
-    return (
-        <div className="game">
-            <GameInfo
-                isDraw={isDraw}
-                winnerSymbol={winnerSymbol}
-                currentStep={currentStep}
-            />
-            <div className="game-field">
-                {cells.map((symbol, index) => (
-                    <GameCell
-                        key={index}
-                        symbol={symbol}
-                        isWinner={winnerSequence?.includes(index)}
-                        onClick={() => handleCellClick(index)}
-                    />
-                ))}
-            </div>
-            <button className="reset" onClick={handleResetClick}>Clear</button>
-        </div>
-    )
+  return (
+    <HomePageLayout header={<Header />}>
+      <UiSelectField
+        label="Label"
+        placeholder="Placeholder"
+        required
+        helperText="Helper text"
+        options={[
+          { label: "Первый label", value: 1 },
+          { label: "Второй label", value: 2 },
+        ]}
+      />
+    </HomePageLayout>
+  );
 }
 
-
-function GameInfo({ isDraw, winnerSymbol, currentStep }) {
-    if (isDraw) {
-        return (
-            <div className="game-info">
-                Draw
-            </div>
-        )
-    }
-
-    if (winnerSymbol) {
-        return (
-            <div className="game-info">
-                Winner: <GameSymbol symbol={winnerSymbol} />
-            </div>
-        )
-    }
-
-    return (
-        <div className="game-info">
-            Turn: <GameSymbol symbol={currentStep} />
-        </div>
-    )
-}
-
-function GameCell({ isWinner, onClick, symbol }) {
-    return (
-        <button
-            className={`cell ${isWinner ? 'cell--win' : ''}`}
-            onClick={onClick}
-        >
-            {symbol ? <GameSymbol symbol={symbol} /> : null}
-        </button>
-    )
-}
-
-function GameSymbol({ symbol }) {
-    const getSymbolClassName = (symbol) => {
-        if (symbol === SYMBOL_O) return 'symbol--o';
-        if (symbol === SYMBOL_X) return 'symbol--x';
-        return '';
-    }
-    return <span className={`symbol ${getSymbolClassName(symbol)}`}>{symbol}</span>
-}
+function HomePageLayout({ header, children }) {
+  return (
+    <div className="bg-slate-50 min-h-screen">
+      {header}
+      <main className="pt-6 mx-auto w-max">{children}</main>
+    </div>
+  );
 }
